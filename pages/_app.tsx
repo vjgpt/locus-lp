c0a9989575e0b2328cdc58a5b6a2a5f1c926b116
vjgpt <vjgupta57@gmail.com>
2022-05-12 19:22:04 +0530

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import '../styles/globals.scss';
 import React from 'react';
+import Script from 'next/script';
 import type { AppProps } from 'next/app';
 
 const LP = function LP({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
   );
 };
 
